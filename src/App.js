80a296e3a2e5d58b99479345a39eb9eb45e011b0
Ruthbai <minjoobai@gmail.com>
2018-05-02 12:08:48 +1200

    form now has fields and it is connected with app.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import './App.css';
 import Form from './Form.js';
 
 class App extends Component {
+
+  state = {
+    fields: {}
+  } //connects to this.state.fields
+
+
+  onSubmit = (fields) => {
+    this.setState({ fields });
+    console.log("App comp got: ", fields);
+  };
+
   render() {
     return (
       <div className="App">
-        <Form />
+        <Form onSubmit={fields => this.onSubmit(fields)}/> {/* props passed here */}
+        <p>{JSON.stringify(this.state.fields, null, 2)}</p> {/* this displays submitted info on page */}
       </div>
     );
   }
--- src/Form.js
+++ src/Form.js
@@ -9,31 +9,100 @@ export default class Form extends React.Component {
         password: ''
     }
 
+
+    change = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onSubmit(this.state); // this connects to App.js on submit it will pass data to app.js
+        this.setState({
+        firstName: '',
+        lastName: '',
+        userName: '',
+        email: '',
+        password: ''//this will clear passwords on screen after submit
+        })
+    }
+
+
     render() {
         return (
             <form>
-            <input placeholder='First name' 
+            <input 
+            name="firstName"
+            placeholder='First name' 
             value={this.state.firstName}  //For the first form box where it says FirstName it will display firstname in state
-            onChange={e => this.setState({ firstName: e.target.value})} //Whatever users type in it will be typed in
+            onChange={e => this.change(e)} //Whatever users type in it will be typed in
             />
-            <input placeholder='Last name'
+            <br />
+            <input 
+            name="lastName"
+            placeholder='Last name'
             value={this.state.lastName} 
-            onChange={e => this.setState({ lastName: e.target.value})}
+            onChange={e => this.change(e)}
             />
-            <input placeholder='User name'
+            <br />
+            <input 
+            name="userName"
+            placeholder='User name'
             value={this.state.userName}
-            onChange={e => this.setState({ userName: e.target.value})}
+            onChange={e => this.change(e)}
             />
-            <input placeholder='Email'
+            <br />
+            <input 
+            name="email"
+            placeholder='Email'
             value={this.state.email}
-            onChange={e => this.setState({ email: e.target.value})}
+            onChange={e => this.change(e)}
             />
-            <input placeholder='Password'
+            <br />
+            <input 
+            name="password"
+            type="password"
+            placeholder='Password'
             value={this.state.password}
-            onChange={e => this.setState({ password: e.target.value})}
+            onChange={e => this.change(e)}
             />
+            <br />
+            <button onClick={e => this.onSubmit(e)}>Submit</button>
             </form>
         );
     }
 }
+
+
+// you can do it this way as well
+
+//     render() {
+//         return (
+//             <form>
+//             <input placeholder='First name' 
+//             value={this.state.firstName}  //For the first form box where it says FirstName it will display firstname in state
+//             onChange={e => this.setState({ firstName: e.target.value})} //Whatever users type in it will be typed in
+//             />
+//             <input placeholder='Last name'
+//             value={this.state.lastName} 
+//             onChange={e => this.setState({ lastName: e.target.value})}
+//             />
+//             <input placeholder='User name'
+//             value={this.state.userName}
+//             onChange={e => this.setState({ userName: e.target.value})}
+//             />
+//             <input placeholder='Email'
+//             value={this.state.email}
+//             onChange={e => this.setState({ email: e.target.value})}
+//             />
+//             <input placeholder='Password'
+//             value={this.state.password}
+//             onChange={e => this.setState({ password: e.target.value})}
+//             />
+//             </form>
+//         );
+//     }
+// }
 
